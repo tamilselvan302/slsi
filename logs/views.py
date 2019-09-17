f5149493ea4e787c479647d58e70e230b4ba5542
Tamil Selvan <tamilselvan302@gmail.com>
2019-09-17 23:13:35 +0530

    third commit

--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
 # Create your views here.
+
+#def handle_uploaded_file(f):
+#    return HttpResponse("")
+
+def home(request):
+	template = loader.get_template('logs/index.html')
+	context = {}
+
+	if request.method == 'POST':
+		#handle_uploaded_file(request.FILES['file'])
+		path = request.POST.get('path',None)
+		return HttpResponse("<h1>"+path+"</h1>")
+	else:
+		return HttpResponse(template.render({},request))
